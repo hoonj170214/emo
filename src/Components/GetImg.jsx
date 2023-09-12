6ba4b7c8dd44c75d853b556eef8bd0e3ef23c43f
hoonj170214 <sh20170214@gmail.com>
2023-09-12 15:05:08 +0900

    api 호출

--- src/Components/GetImg.jsx
+++ src/Components/GetImg.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { getImgById, imgList } from '../util';
 
-function GetImg() {
+function GetImg({ prop }) {
   return (
     <div>
       <div>
-        {imgList.filter((it) => (
+        {/***  {imgList.filter((it) => (
           <img key={it.id} src={it.img} alt={`img${it.id}`} />
-        ))}{' '}
+      ))}{' '} **/}
       </div>
     </div>
   );
--- src/Pages/driverStation.jsx
+++ src/Pages/driverStation.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GetImg from '../Components/GetImg';
 
 function DriverStation() {
-  return (
-    <div>
-      <GetImg />
-    </div>
-  );
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const key = `serviceKey=97eabseJjXsRuTOaYAkcZ%2FPXibvwipHJKBay3VS%2BDW6STV5QjDDoxIsPxGdK1KCaBMV3MFoR8T%2BSt3JvufMhSg%3D%3D`;
+      const addr = `{}`;
+      const res = await fetch(
+        `https://api.odcloud.kr/api/EvInfoServiceV2/v1/getEvSearchList?page=1&perPage=10&returnType=JSON&${key}`
+      );
+      const result = res.json();
+      console.log(result);
+      const responsData = await result.then((result) => result.data);
+      console.log(responsData);
+      setData(responsData);
+    };
+    fetchData();
+    console.log(data);
+  }, []);
+
+  console.log(data);
+  return <div>{data && <p>{JSON.stringify(data, null, 2)}</p>}</div>;
 }
 
 export default DriverStation;
